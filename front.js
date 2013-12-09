d46c40513a068780de3a6746cf84b2556d3d3fbd
bmoren <ben@benmoren.com>
2013-12-08 23:08:07 -0600

    working...adding...building....

---
+++ front.js
@@ -0,0 +1,16 @@
+//Lets get things going
+$(document).ready(function() {
+
+$("#infoBox").hide();
+
+$("buttonClick").click(function () {
+	$(".showhidebox").fadeIn("slow");
+    });
+
+$(".showhideclose").click(function () {
+	$(".showhidebox").fadeOut("slow");
+    });
+
+}); // end of document ready
+
+
