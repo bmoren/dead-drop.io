75b658de1c037244bb7174ad3825dc5cbfe5f70d
Derek Anderson <derek@mediaupstream.com>
2013-12-09 21:11:31 -0600

    Image uploading via drag/drop and the Node stuff to handle sharing

--- app.js
+++ app.js
@@ -1,29 +1,101 @@
 // Load libraries
 var express = require('express'),
+  fs = require('fs'),
 	app = express(),
+  messages = [],
+  downloads = [],
   port = 8999;
+
+require('sugar') // yum!
 
 // Setup Express Middleware
 app.set('views', __dirname );
 app.engine('html', require('ejs').renderFile);
+
+//
+// Middleware to check for downloaded things
+//
+app.use(function(req, res, next){
+  // ATTEMPTING TO DOWNLOAD SOME SHIT!!!
+  if (req.url.indexOf('/img/uploads/') != -1){
+    if (downloads.any( req.url )){
+      downloads.remove( req.url ) // user downloaded this now we can remove it!
+      // actually remove the image
+      var url = path.normalize( __dirname + '/public' + req.url )
+      fs.unlink(url, function() {
+        return next()
+      });
+    }
+    return res.send('NOPE', 404);
+  }
+  next();
+})
+
 app.use(express.bodyParser());
 app.use(express.cookieParser('some secret'));
+
 app.use(express.static('public'));
 
-var message = "empty";
+// Message Object
+var Message = function(type, url){
+  this.type = type || 'image';
+  this.url = url || '/img/uploads/1st.jpg';
+}
+
+// Initialize our messages and download lists
+messages.push( new Message('image', '/img/uploads/1st.jpg') )
+downloads.push( messages[0].url )
+
+//
+// Upload Images method
+// @param {object} file - a file upload object from: req.files.file for example
+// @param {function} cb - a callback receives (error, new file url)
+// 
+var uploadImage = function(file, cb){
+  var tmp_path = file.path;
+  var img = '/public/img/uploads/' + file.name;
+  var target_path = __dirname + img;
+  fs.rename(tmp_path, target_path, function(err) {
+    if (err) return cb(err)
+    fs.unlink(tmp_path, function() {
+      cb(null, img)
+    })
+  })
+}
+
+//
+// Add a new Message and receive a new message in response
+// @returns response message
+//
+var addNew = function(type, url){
+  var message = new Message(type, url)      // create a new url 'message'
+  var response = messages.pop()             // grab the last 'message'
+  messages.push(message)                    // add the new 'message'
+  downloads.push( message.url )             // add to list of available downloads
+  return response                           // return the previously added 'message'
+}
 
 
-
+//
+// Share endpoint receives url or file uploads and does stuff
+//
 app.post('/share', function(req, res) {
-  var data = req.body;
-  //return res.send('thanks for sharing');
 
-  var nextmessage = message;
+  // User is trying to upload a url
+  if (req.body.image){
+    return res.json( { message: addNew('url', req.body.image) } )
+  }
 
-  message = data.image;
+  // User is trying to upload a file (maybe?)
+  if (!req.files || !req.files.file) return res.json({error: 'Something went wrong'});
 
-  //res.send( message);
-  res.send( '<img src="' + nextmessage + '"/ >');
+  uploadImage( req.files.file, function(err, url){
+    if (err) {
+      console.log( err );
+      return res.json({error: 'Something else went wrong'})      
+    }
+    return res.json( { message: addNew('image', url) } );
+  });
 });
 
 
--- public/js/front.js
+++ public/js/front.js
@@ -1,18 +1,37 @@
 //Lets get things going
 $(document).ready(function() {
 
-$(".infoBox").hide();
+  $(".infoBox").hide();
 
-$("buttonClick").click(function () {
-	$(".showhidebox").fadeIn("slow");
-    });
+  $("buttonClick").click(function () {
+  	$(".showhidebox").fadeIn("slow");
+      });
 
-$(".showhideclose").click(function () {
-	$(".showhidebox").fadeOut("slow");
-});
+  $(".showhideclose").click(function () {
+  	$(".showhidebox").fadeOut("slow");
+  });
 
-// Dropzone stuff
-var myDropzone = new Dropzone("div#background", { url: "/share"});
+  // Dropzone stuff
+  Dropzone.options.background = {
+    maxFilesize: 100,
+    parallelUploads: 1,
+    uploadMultiple: false,
+    addRemoveLinks: false,
+    previewsContainer: '#no-previews',
+    clickable: false,
+    createImageThumbnails: false,
+    acceptedFiles: 'image/*,text/plain,text/csv,text/x-markdown',
+    accept: function(file, done) {
+      // console.log( file )
+      done()
+    }
+  }
+  var myDropzone = new Dropzone("div#background", { url: "/share"});
+
+  myDropzone.on("success", function(file, resp) {
+    console.log( 'uploaded file', resp )
+    // create overlay here...
+  })
 
 
 }); // end of document ready
