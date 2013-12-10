d6b61343a522c9e1ad9ce06a7059f6cecb73b1d9
bmoren <ben@benmoren.com>
2013-12-09 20:36:23 -0600

    Overlays added, overlay image responsive

--- public/js/front.js
+++ public/js/front.js
@@ -1,16 +1,42 @@
 //Lets get things going
 $(document).ready(function() {
 
-$(".infoBox").hide();
-
+//Hides
+$("#infoBox").hide();
+ $(".overlayWhite").hide();
+ $(".overlayTrans").hide();
+ $("#overlayContent").hide();
+ $("#contentClose").hide();
+//infoBox stuff
 $("#infoButton").click(function () {
-	$(".infoBox").fadeIn("slow");
+	$(".overlayTrans").fadeIn("slow").css("z-index, 100");
+	$("#infoBox").fadeIn("slow").css("z-index, 200");
     });
 
 $("#infoClose").click(function () {
-	$(".infoBox").fadeOut("slow");
+	$("#infoBox").fadeOut("slow");
+	$(".overlayTrans").fadeOut("slow");
+    });
+//infoBox stuff
+
+//content overlay stuff
+$("#testing").click(function () {
+	$("#overlayContent").fadeIn("slow").css("z-index, 200");
+	$(".overlayWhite").fadeIn("slow").css("z-index, 100");
+	$("#contentClose").fadeIn("slow").css("z-index, 200");
+	});
+
+$("#contentClose").click(function () {
+	$("#overlayContent").fadeOut("slow");
+	$(".overlayWhite").fadeOut("slow");
+	$("#contentClose").fadeOut("slow");
     });
 
+
+//content overlay stuff
+
 }); // end of document ready
+
+
 
 
