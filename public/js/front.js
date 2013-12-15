a0e0881ce9fa7f8db129ae68ba005d3605704b98
bmoren <ben@benmoren.com>
2013-12-15 13:06:06 -0600

    added functions for embeda

--- public/js/front.js
+++ public/js/front.js
@@ -29,7 +29,8 @@ $(document).ready(function() {
 
     var pushImageOut = resp.message.url ;                             //get the URL of the 'old' image
     pushImageOut = pushImageOut.substring(8, pushImageOut.length);    //remove '/public/'
-    contentOverlay(pushImageOut);                                     //cue overlay with the 'old' image
+    //imageOverlay(pushImageOut);                                     //cue overlay with the 'old' image
+    youtubeOverlay('rE6pwmHHVb0');
   })
 
 
@@ -51,13 +52,42 @@ $("#infoClose").click(function () {
     });
 //infoBox stuff
 
-//content overlay stuff
-function contentOverlay(theThing) {
-  $("#overlayContent").html("<img src=" + "'" + theThing + "'" + "/>");
-	$("#overlayContent").fadeIn("slow").css("z-index, 200");
-	$(".overlayWhite").fadeIn("slow").css("z-index, 100");
-	$("#contentClose").fadeIn("slow").css("z-index, 200");
+//content injection stuff
+function imageOverlay(imageURL) {
+  $("#overlayContent").html("<img src=" + "'" + imageURL + "'" + "/>");
+  contentOpen();
 	};
+
+function youtubeOverlay(youtubeID) {
+  $("#overlayContent").html('<iframe src="http://www.youtube.com/embed/' + youtubeID + '?rel=0" frameborder="0" allowfullscreen></iframe>').css("z-index, 9999");
+  contentOpen();
+  };
+
+function vimeoOverlay(vimeoID) {
+  $("#overlayContent").html('<iframe src="http://player.vimeo.com/video/' + vimeoID + '?portrait=0&amp;color=ffffff" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>').css("z-index, 9999");
+  contentOpen();
+  };
+
+function soundcloudOverlay(soundcloudID) {
+  $("#overlayContent").html('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + soundcloudID + '&amp;color=000&amp;auto_play=true&amp;show_artwork=true"></iframe>');
+  contentOpen();
+  };
+
+  function bandcampOverlay(bandcampID) {
+  $("#overlayContent").html('<iframe style="border: 0; width: 350px; height: 470px;" src="http://bandcamp.com/EmbeddedPlayer/album=' + bandcampID + '/size=large/bgcol=ffffff/linkcol=333333/notracklist=true/t=1/transparent=true/" seamless>');
+  contentOpen();
+  };
+
+
+//content overlay stuff
+
+function contentOpen(){
+  $("#overlayContent").fadeIn("slow").css("z-index, 200");
+  $(".overlayWhite").fadeIn("slow").css("z-index, 100");
+  $("#contentClose").fadeIn("slow").css("z-index, 200");
+
+}
+
 
 $("#contentClose").click(function () {
 	$("#overlayContent").fadeOut("slow");
@@ -66,7 +96,10 @@ $("#contentClose").click(function () {
     });
 
 
-//content overlay stuff
+
+
+
+
 
 }); // end of document ready
 
