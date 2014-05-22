304156320062e3ee2c828169d7bd0cf5729d15ab
bmoren <ben@benmoren.com>
2014-05-21 22:08:19 -0500

    fishing flash message stuff

--- public/js/front.js
+++ public/js/front.js
@@ -238,7 +238,7 @@ $(document).ready(function() {
   //bandcampOverlay('1171202479');
   //bandcampOverlay('456502597');
 
-  //flashMessage('this is a test');
+  flashMessage('this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a test');
 
   //content overlay stuff
   function contentOpen(content){
