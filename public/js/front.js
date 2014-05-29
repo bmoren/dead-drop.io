0727ca7176a78ac5a3f716e1684565e8e28679b7
bmoren <ben@benmoren.com>
2014-05-28 20:36:24 -0500

    testing deploy

--- public/js/front.js
+++ public/js/front.js
@@ -3,6 +3,8 @@ var DEFAULT_INPUT_VAL = 'paste link or drag and dead-drop';
 //Lets get things going
 $(document).ready(function() {
 
+  console.log('the deploy worked on try #3');
+
   var _host = 'http://'+ window.location.hostname;
 
   // donation stuff
